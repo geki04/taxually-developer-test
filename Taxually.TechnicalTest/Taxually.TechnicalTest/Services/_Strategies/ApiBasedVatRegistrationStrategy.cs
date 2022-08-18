46b090a1559f6344420b664b121c62e0a677740c
BETSSON\geki04 <geki04@betssongroup.com>
2022-08-18 09:25:59 +0200

    [TXLY-238] Implementation of API based VAT registration strategy

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/_Strategies/ApiBasedVatRegistrationStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Taxually.TechnicalTest.Contracts;
+using Taxually.TechnicalTest.Interfaces;
+
+namespace Taxually.TechnicalTest.Services
+{
+    /// <summary>
+    /// Responsible for registering a company for a VAT number using an API
+    /// </summary>
+    public class ApiBasedVatRegistrationStrategy : IVatRegistrationStrategy
+    {
+        private readonly string apiAddress;
+        private readonly VatRegistrationRequest request;
+
+        public ApiBasedVatRegistrationStrategy(VatRegistrationRequest request, string apiAddress)
+        {
+            this.request = request;
+            this.apiAddress = apiAddress;
+        }
+
+        public async Task CalculateVatAsync()
+        {
+            // UK has an API to register for a VAT number
+            var httpClient = new TaxuallyHttpClient();
+
+            await httpClient.PostAsync(apiAddress, request);
+        }
+    }
+}
