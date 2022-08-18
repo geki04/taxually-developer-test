a4662674f44f3ccfd3e13e3a1e39e472107e8770
BETSSON\geki04 <geki04@betssongroup.com>
2022-08-18 09:20:07 +0200

    [TXLY-238] Added VAT registration validator

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/_Validator/VatRegistrationValidator.cs
@@ -0,0 +1,28 @@
+﻿using Taxually.TechnicalTest.Contracts;
+using Taxually.TechnicalTest.Interfaces;
+
+namespace Taxually.TechnicalTest.Services
+{
+    /// <summary>
+    /// Responsible for validating a VAT registration request.
+    /// </summary>
+    public class VatRegistrationValidator : IVatRegistrationValidator
+    {
+        public bool Validate(VatRegistrationRequest request)
+        {
+            if (request == null)
+            {
+                return false;
+            }
+
+            if (request.Country == Country.None)
+            {
+                return false;
+            }
+
+            // and so on...
+
+            return true;
+        }
+    }
+}
