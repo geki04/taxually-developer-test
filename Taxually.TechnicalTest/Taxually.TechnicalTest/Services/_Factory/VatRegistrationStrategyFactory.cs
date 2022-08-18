668eec5346b84ec987a843857b0ae5eea9b8de8b
BETSSON\geki04 <geki04@betssongroup.com>
2022-08-18 09:45:25 +0200

    [TXLY-238] Added VatRegistration factory for currently supported

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/_Factory/VatRegistrationStrategyFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Taxually.TechnicalTest.Contracts;
+using Taxually.TechnicalTest.Services;
+
+namespace Taxually.TechnicalTest.Interfaces
+{
+    public class VatRegistrationStrategyFactory : IVatRegistrationStrategyFactory
+    {
+        public IVatRegistrationStrategy FindVatStrategy(VatRegistrationRequest request) => request.Country switch
+        {
+            Country.GB => new ApiBasedVatRegistrationStrategy(request, "https://api.uktax.gov.uk"),
+
+            Country.FR => new CsvBasedVatRegistrationStrategy(request, "vat-registration-csv"),
+
+            Country.DE => new XmlBasedVatRegistrationStrategy(request, "vat-registration-xml"),
+
+            _ => throw new NotSupportedException(
+                    $"Not suppported Country for VAT registration: {request.Country}"),
+        };
+    }
+}
