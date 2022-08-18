f0312b8205f2b797efe0dd5adc66797dfc943d0a
BETSSON\geki04 <geki04@betssongroup.com>
2022-08-18 09:38:22 +0200

    [TXLY-238] Implementation of XML-based VAT registration strategy

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Services/_Strategies/XmlBasedVatRegistrationStrategy.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Taxually.TechnicalTest.Contracts;
+using Taxually.TechnicalTest.Interfaces;
+
+namespace Taxually.TechnicalTest.Services
+{
+    /// <summary>
+    /// Responsible for registering a company for a VAT number using an XML
+    /// </summary>
+    public class XmlBasedVatRegistrationStrategy : IVatRegistrationStrategy
+    {
+        private readonly string queueName;
+        private readonly VatRegistrationRequest request;
+
+        public XmlBasedVatRegistrationStrategy(VatRegistrationRequest request, string queueName)
+        {
+            this.request = request;
+            this.queueName = queueName;
+        }
+
+        public async Task CalculateVatAsync()
+        {
+            // Germany requires an XML document to be uploaded to register for a VAT number
+            using (var stringWriter = new StringWriter())
+            {
+                var serializer = new XmlSerializer(typeof(VatRegistrationRequest));
+                serializer.Serialize(stringWriter, this.request);
+
+                var xml = stringWriter.ToString();
+                var xmlQueueClient = new TaxuallyQueueClient();
+
+                // Queue xml doc to be processed
+                await xmlQueueClient.EnqueueAsync(this.queueName, xml);
+            }
+        }
+    }
+}
